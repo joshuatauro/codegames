ecbd039652d0f674e17179cf91376c059ea31db9
joshuatauro <joshuatauro45@gmail.com>
2022-12-25 16:04:08 +0530

    fixed buttons link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../CSS/Navbar.css'
 const Navbar = () => {
   return (
     <nav className="cg__nav">
       <div className="cg__nav__container">
-        <h1 className="cg__nav__title">DeCoders</h1>
+        <Link className="cg__nav__title" to="/">
+          <h1 >DeCoders</h1>
+        </Link>
         <div className="ui cg__nav__links">
           <li className="cg__nav__link"><h1>About</h1></li>
           <li className="cg__nav__link"><h1>Games</h1></li>
           <li className="cg__nav__link"><h1>Contact</h1></li>
         </div>
-        <button className="cg__nav__btn">Register!</button>
+        <Link to="/register" className="cg__nav__btn">Register!</Link>
       </div>
     </nav>
   )
